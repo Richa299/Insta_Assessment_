76730f0cb168dbb7c2b8ccb0624f4f0b966ba9e6
Richa299 <richa.singhnxt213@gmail.com>
2023-01-16 11:23:27 +0530

    post section created and display the images

--- src/components/Footer/Posts/Posts.jsx
+++ src/components/Footer/Posts/Posts.jsx
@@ -1,5 +1,8 @@
 import { useState } from 'react'
 import { Container } from 'react-bootstrap'
+import { AiOutlineTable } from 'react-icons/ai';
+import { BiMoviePlay } from 'react-icons/bi'
+import { FaIdBadge } from 'react-icons/fa'
 import './Posts.css'
 import SinglePost from '../SinglePost/SinglePost'
 function Posts() {
@@ -12,22 +15,28 @@ function Posts() {
 
 
     return (
-        <Container>
-            <span className='post-option'>posts</span>
-            <span>posts</span>
-            <span>posts</span>
-
+        <Container className='post-container'>
+            <div className="post-option">
+                <button>
+                    <AiOutlineTable/>POSTS
+                </button>
+                <button>
+                    <BiMoviePlay />REELS
+                </button>
+                <button>
+                    <FaIdBadge />TAGGED
+                </button>
+            </div>
             <div className="post-section">
-            
                 {data.map((item, index) => {
                     return (
-                        <SinglePost url={item.thumbnailUrl}/>
+                        <SinglePost url={item.thumbnailUrl} />
                     )
-                 })}
-            
+                })}
+
             </div>
         </Container>
     )
 
 }
-                export default Posts
+export default Posts
